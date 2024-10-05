c6fda374b8cf4a5a926fb915d1790a216fe3b13f
Alex <alexdesouzasilva94@gmail.com>
2024-10-05 13:23:33 -0300

    add

---
+++ nexus.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mvn=$(which mvn)
+worke_dir=$(pwd)
+version=$($mvn -f "$worke_dir/pom.xml" help:evaluate -Dexpression=project.version -q -DforceStdout) || echo "Falha ao capturar version do pom.xml" exit 0
+groupId=$($mvn -f "$worke_dir/pom.xml" help:evaluate -Dexpression=project.groupId -q -DforceStdout) || echo "Falha ao capturar groupId do pom.xml" exit 0
+artifactId=$($mvn -f "$worke_dir/pom.xml" help:evaluate -Dexpression=project.artifactId -q -DforceStdout) || echo "Falha ao capturar artifactId do pom.xml" exit 0
+repositoryId="devops-maven-snapshot"
+
+mvn deploy -B \
+    -Durl=http://localhost:8081 \
+    -Dfile=target/$artifactId.jar \
+    -DgroupId=$groupId \
+    -DartifactId=$artifactId \
+    -Dpackaging=jar \
+    -Dversion=$version \
+    -DrepositoryId=$repositoryId
